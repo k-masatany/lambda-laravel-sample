56141495e19a95930e526d4bf8ad498b3f58fa9a
k-masatany <masatani@fusic.co.jp>
2019-06-03 08:09:29 +0900

    reset

--- php-layer-for-laravel5/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-MD5SUM=$(md5 -q php71.zip)
-S3KEY="php71-laravel/${MD5SUM}"
-region="ap-northeast-1"
-bucket_name="stackery-layers.k-masatany"
-
-echo "Uploading php71.zip to s3://${bucket_name}/${S3KEY}"
-aws --region $region s3 cp php71.zip "s3://${bucket_name}/${S3KEY}"
